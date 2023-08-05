c851b0f4ea2e10e3775e16406d36cf0d0ffda8b7
Anthony <ajgillesp@outlook.com>
2023-08-04 21:13:54 -0400

    complete.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, adj){
+    let thank = [];
+    for (let x = 0; x < names.length; x++){
+        thank[x] = `Thank you, ${names[x]}, for the wonderful ${adj} gift!`;   
+    }
+    return thank;
+};
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n);
+        n--;
+    }
+};
